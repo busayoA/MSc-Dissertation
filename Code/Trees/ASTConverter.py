cae39d3ac43860339a72919c4de32b1f8c7e84fd
Busayo Akeredolu <45513100+busayoA@users.noreply.github.com>
2022-02-21 09:47:56 +0000

    Minor Updates

--- Code/Trees/ASTConverter.py
+++ Code/Trees/ASTConverter.py
@@ -21,15 +21,57 @@ class ASTConverter():
         with open(self.inputFile, "r") as source:
             self.astTree = source.readlines()
 
-
     def generateTree(self):
         root = Node(self.astTree[0])
-        maxLen = len(self.astTree)
-        spaces = list(set([len(i) - len(i.lstrip()) for i in self.astTree]))
-        treeDepth = len(spaces)
-        print(treeDepth)
+        treeLength = len(self.astTree)
+        spaces = [len(i) - len(i.lstrip()) for i in self.astTree]
+        treeIndexes, trees = [], []
+
+        for i in range(len(self.astTree)):
+            if spaces[i] == 0:
+                treeIndexes.append(i)
+
+        #Basic Implementation made to work with code containing only two methods for the purposes of this project
+        for i in range(len(treeIndexes)-1):
+            i = treeIndexes[i]
+            j = treeIndexes[i+1]
+            trees.append([self.astTree[i:j]])
+            trees.append([self.astTree[j:]])
+
+        treeLevels = []
+        for i in range(len(trees)):
+            root = trees[i][0][0]
+            root = Node(root)
+            treeSpaces = [len(i) - len(i.lstrip()) for i in trees[i][0]]
+            nodes = []
+            treeSet = list(sorted(set(treeSpaces)))
+            treeDepth = len(treeSet)
+
+            for j in treeSet:
+                thisLevel = []
+                for k in range(len(trees[i][0])):
+                    innerLevel = []
+                    if spaces[k] == j:
+                        thirdLevel = []
+                        thirdLevel.append(trees[i][0][k])
+                        # print(trees[i][0][k])
+            #         innerLevel = []
+            #         if spaces[k] == j:
+            #             innerLevel.append(spaces[k])
+
+            #     thisLevel.append(innerLevel)
+            # treeLevels.append(thisLevel)
+            # for i in trees[i][0][1:]:
+            #     count = max(treeSpaces)
+            #     j = 4
+            #     while j <= count:
+            #         if spaces[i] == j :
+            #             newNode = 
 
 
+            # root = Node(root)
+
+                print(thirdLevel)
 
 
         #     astTree = ast.parse(source.read()) #converts the code to a tree
@@ -41,7 +83,7 @@ class ASTConverter():
         # l = l.split("body")
         # [print(i, end="\n\n") for i in l]
 
-testFile = "/Users/olubusayoakeredolu/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Dissertation/Code/Training Data/Bubble Sort/bs1.py"
+testFile = "/Users/olubusayoakeredolu/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Dissertation/Code/Training Data/Quick Sort/qs1.py"
 astConverter = ASTConverter(testFile)
 astConverter.readInputFile()
 astConverter.generateTree()
--- Code/Trees/Node.py
+++ Code/Trees/Node.py
@@ -1,9 +1,7 @@
-from anytree import AnyNode
-
-class Node(AnyNode):
-    def __init__(self, nodeName, declarations):
+class Node():
+    def __init__(self, nodeName, contents):
         self.id = nodeName
-        self.declarations = declarations
-        print(self.id, self.declarations)
+        self.contents = contents
+        print(self.id, self.contents)
 
 n = Node("root", ["swap = true"])
