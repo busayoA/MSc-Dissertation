7a781af20be65092f6f6e50bf724e6d467b08f16
Busayo Akeredolu <45513100+busayoA@users.noreply.github.com>
2022-02-21 17:08:13 +0000

    Started working on RNN

--- Code/Neural Networks/RNN.py
+++ Code/Neural Networks/RNN.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python3
 
 import tensorflow as tf
+import numpy as numpy
+print(tf.__version__)
 
-class GNN:
+class RNN:
     def __init__(self, layers):
         self.layers = layers
