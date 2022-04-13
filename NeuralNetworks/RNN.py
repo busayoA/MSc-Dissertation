02803fd60b61a9a3f96ca6b4456c0dd4317cc676
Busayo Akeredolu <45513100+busayoA@users.noreply.github.com>
2022-04-13 19:03:50 +0100

    Hard Reset on RNN

--- NeuralNetworks/RNN.py
+++ NeuralNetworks/RNN.py
@@ -18,59 +18,49 @@ class RNN:
         self.db = {}
 
         for i in range(1, self.L):
-            self.W[i] = tf.Variable(tf.random.normal(shape=(105, 105)))
-            self.b[i] = tf.Variable(tf.random.normal(shape=(100, 105)))
+            self.W[i] = [[random.random() for y in range(self.layers[i-1])] for x in range(self.layers[i])]
+            self.b[i] = [[random.random() for y in range(self.layers[i])] for x in range(self.layers[i])]
         
-    def forward_pass(self, A):
-        A = tf.convert_to_tensor(A, dtype=tf.float32)
-        for i in range(1, self.L):
-            Z = tf.matmul(A, tf.transpose(self.W[i])) + self.b[i]
-            if i != self.L-1:
-                A = tf.nn.relu(Z)
-            else:
-                A = Z
-        return A
+    def forward_pass(self, x_train):   
+        for feature in x_train:
+            inputs = feature
+            for i in range(1, self.L):
+                currentLayer = self.layers[i]
+                self.W[i] = np.array(self.W[i])
+                self.b[i] = np.array(self.b[i])
+                finalValues = []
+                index = 0
+                for j in range(currentLayer):
+                    weight = self.W[i][j][index]
+                    bias = self.b[i][j][index]
+                    activation = 0.
+                    for k in range(len(inputs)):
+                        activation += (inputs[k] * weight) + bias
+                    index += 1
+                    finalValues.append((tf.nn.relu(activation/256.0).numpy()))
+                inputs = finalValues
+            if currentLayer == 5:
+                print(inputs)
+                
+                # finalValues.append(tf.matmul(A, tf.transpose(self.W[i])) + tf.transpose(self.b[i]))
+            
+            # if i != self.L-1:
+            #     A = tf.nn.relu(Z)
+            # else:
+            #     A = Z
+        return inputs
 
     def compute_loss(self, A, Y):
         return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.transpose(Y), tf.transpose(A)))
     
-    def update_params(self, lr):
+    def updateWeights(self, lr):
         for i in range(1, self.L):
-            self.W[i].assign_sub(lr * self.dW[i])
-            self.b[i].assign_sub(lr * self.db[i])
+            self.W[i] = self.W[i] - (lr * self.W[i])
+            self.b[i] = self.b[i] - (lr * self.b[i])
 
-    def predict(self, X):
-        A = self.forward_pass(X)
-        return tf.argmax(tf.nn.softmax(A), axis=1)
-    
-    def info(self):
-        num_params = 0
-        for i in range(1, self.L):
-            num_params += self.W[i].shape[0] * self.W[i].shape[1]
-            num_params += self.b[i].shape[0]
-        print('Input Features:', self.num_features)
-        print('Number of Classes:', self.num_classes)
-        print('Hidden Layers:')
-        print('--------------')
-        for i in range(1, self.L-1):
-            print('Layer {}, Units {}'.format(i, self.layers[i]))
-        print('--------------')
-        print('Number of parameters:', num_params)
-
-    def train_on_batch(self, X, Y, lr):
-        X = tf.convert_to_tensor(X, dtype=tf.float32)
-        Y = tf.convert_to_tensor(Y, dtype=tf.float32)
-        
-        with tf.GradientTape(persistent=True) as tape:
-            A = self.forward_pass(X)
-            loss = self.compute_loss(A, Y)
-        for key in self.W.keys():
-            self.dW[key] = tape.gradient(loss, self.W[key])
-            self.db[key] = tape.gradient(loss, self.b[key])
-        del tape
-        self.update_params(lr)
-        
-        return loss.numpy()
+    def predict(self, featureValues):
+         outputs = self.forward_pass(featureValues)
+         return np.where(outputs == np.amax(outputs))
 
     def train(self, x_train, y_train, x_test, y_test, epochs, steps_per_epoch, batch_size, lr):
         history = {
@@ -82,23 +72,26 @@ class RNN:
         for e in range(0, epochs):
             epoch_train_loss = 0.
             print('Epoch {}'.format(e), end='.')
-            for i in range(0, steps_per_epoch):
-                x_batch = x_train[i*batch_size:(i+1)*batch_size]
-                y_batch = y_train[i*batch_size:(i+1)*batch_size]
-                batch_loss = self.train_on_batch(x_batch, y_batch, lr)
-                epoch_train_loss += batch_loss
-                print(epoch_train_loss)
-                
-                if i%int(steps_per_epoch/10) == 0:
-                    print(end='.')
-                    
-            history['train_loss'].append(epoch_train_loss/steps_per_epoch)
+            for i in range(5):
+                outputs = self.forward_pass(x_train[:3])
+                # batch_loss = self.train_on_batch(x_train, y_train, lr)
+                # epoch_train_loss += batch_loss
+                print(end='.')
+            self.updateWeights(lr)
+
+            validationAccuracy = 0
+            for i in range(len(x_test[:5])):
+                prediction = self.predict(x_test[:5])
+                if (prediction[0][0] == y_test[i]):
+                    validationAccuracy += 1
+            print("Accuracy:", validationAccuracy/len(x_test))
+            # history['train_loss'].append(epoch_train_loss/steps_per_epoch)
             
-            x_test = tf.convert_to_tensor(x_test[:100], dtype=tf.float32)
-            val_A = self.forward_pass(x_test)
-            history['val_loss'].append(self.compute_loss(val_A, y_test[:100]).numpy())
+            # # x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
+            # val_A = self.forward_pass(x_test)
+            # history['val_loss'].append(self.compute_loss(val_A, y_test).numpy())
             
-            val_preds = self.predict(x_test)
-            history['val_acc'].append(np.mean(np.argmax(y_test[:100]) == val_preds.numpy()))
-            print('Val Acc:', history['val_acc'][-1], 'Loss:', history['train_loss'][-1])
+            # val_preds = self.predict(x_test)
+            # history['val_acc'].append(np.mean(np.argmax(y_test) == val_preds.numpy()))
+            # print('Val Acc:', history['val_acc'][-1], 'Loss:', history['train_loss'][-1])
         return history
--- NeuralNetworks/trainTextModel.py
+++ NeuralNetworks/trainTextModel.py
@@ -2,50 +2,46 @@ import pandas as pd
 import RNN as NN
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
 # GET THE DATA/TEXT 
 # with open("/Users/olubusayoakeredolu/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Dissertation/Data/Corona_NLP_train.csv") as file:
 trainingData = pd.read_csv("/Users/olubusayoakeredolu/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Dissertation/Data/Corona_NLP_train.csv", encoding = "ISO-8859-1")
 testingData = pd.read_csv("/Users/olubusayoakeredolu/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Dissertation/Data/Corona_NLP_test.csv", encoding = "ISO-8859-1")
+
+
 sentimentLabel = {'Extremely Negative': 0, 'Negative': 1, 'Neutral': 2, 'Positive': 3, 'Extremely Positive': 4}
 trainingData['Sentiment'] =  [sentimentLabel[item] for item in trainingData['Sentiment']]
 testingData['Sentiment'] =  [sentimentLabel[item] for item in testingData['Sentiment']]
 
 # lines = [line.strip() for line in lines] 
 # df1 = 
-x = list(trainingData['OriginalTweet'])
-y = list(trainingData['Sentiment'])
+x_train = trainingData['OriginalTweet']
+y_train = trainingData['Sentiment']
 
-# x_test = list(testingData['OriginalTweet'])
-# y_test = list(testingData['Sentiment'])
+x_test = testingData['OriginalTweet']
+y_test = testingData['Sentiment']
 
-# print(lines)
+vectorizer = CountVectorizer(tokenizer=lambda doc: doc, min_df=2)
+vectorizer.fit(x_train)
 
+x_train = vectorizer.transform(x_train)
+x_test  = vectorizer.transform(x_test)
 
-def createEmbeddings(xData):
-    embeddings = CountVectorizer(tokenizer=lambda doc: doc, min_df=2).fit_transform(xData)
-    embeddings = embeddings.toarray()
-    return embeddings
+print(x_train)
+x_train = x_train.toarray()
+x_test = x_test.toarray()
+# x_train = np.reshape(x_train, (x_train.shape[0], len(x_train[0])))/255.
+# x_test =  np.reshape(x_test, (x_test.shape[0], len(x_test[0])))/255.
 
-x = createEmbeddings(x)
-x_train = x[:int(0.7*len(x))]
-x_test = x[int(0.7*len(x)):]
-x_train = np.reshape(x_train, (x_train.shape[0], len(x_train[0])))/255.
-x_test =  np.reshape(x_test, (x_test.shape[0], len(x_test[0])))/255.
-
-
-y_train = y[:int(0.7*len(y))]
-y_test = y[int(0.7*len(y)):]
-
-print(len(y_train))
-print(len(y_test))
 
 epochs = 5
 lr = 0.4
 batch_size = 100
-steps_per_epoch = 50
+steps_per_epoch = 5
 
-rnn  = NN.RNN([len(x_train), 100, 100, 5])
-rnn.info()
+rnn  = NN.RNN([len(x_train), 128, 128, 5])
+# rnn.info()
 print('Steps per epoch:', steps_per_epoch)
 history = rnn.train(
     x_train, y_train,
@@ -53,11 +49,3 @@ history = rnn.train(
     epochs, steps_per_epoch,
     batch_size, lr
 )
-# # rnn.trainModel(x_train, y_train, x_test, y_test)
-# # for i in range(len(x_train[:60])):
-# #     prediction = rnn.predict(x_train[i])
-# #     print("Expected=", y_train[i], " predicted", prediction[0][0])
-
-# # for i in range(len(x_test)):
-# #     prediction2 = rnn.predict(x_test[i])
-# #     print("Expected=", y_test[i], " predicted", prediction2[0][0])
